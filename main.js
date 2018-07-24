e816294cf4fe368dd2e38b59ebbd8ee753eafd74
cain910772 <cain910772@yahoo.com>
2018-07-23 21:39:47 -0500

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+function addExcitement (theWords) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
+    let makeSentence = ""
+
+    for (let i = 0; i < sentence.length; i++) {
+        addExcitement +=  i[0] + 1;
+        console.log(theWords);
+        
+        
+
+        // Print buildMeUp to the console
+    }
+
+}
+
+// Invoke the function and pass in the array
+ addExcitement(sentence);
